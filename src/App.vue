4752db9951508bea2dd8e71b79427b875312c8e6
renanferreirayassumoto <renankosmo@gmail.com>
2023-10-06 10:40:54 -0400

    classes de utilidade

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-app-bar flat class="border-b">
+      <v-app-bar-title>Meu app</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <div class="d-flex justify-space-between">
+          <div style="width: 100px; height: 100px" class="border rounded-lg elevation-2">
+            teste
+          </div>
+          <div style="width: 100px; height: 100px" class="border rounded-lg elevation-2">
+            teste
+          </div>
+          <div style="width: 100px; height: 100px" class="border rounded-lg elevation-2">
+            teste
+          </div>
+        </div>
+      </v-container>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -8,19 +27,13 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
