c19e3a2cb244b9951f77781388c6be1ad08e1d8c
renanferreirayassumoto <renankosmo@gmail.com>
2023-10-06 10:55:03 -0400

    Estilos

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,23 @@
     </v-app-bar>
     <v-main>
       <v-container>
-        <div class="d-flex justify-space-between">
-          <div style="width: 100px; height: 100px" class="border rounded-lg elevation-2">
+        <h1 class="text-h6">Meu app</h1>
+        <h2>Meu app</h2>
+        <h3>Meu app</h3>
+        <h4>Meu app</h4>
+        <h5>Meu app</h5>
+        <h6>Meu app</h6>
+        <br><br>
+        <div class="text-subtitle-2">Subtitle 1</div>
+        <div class="text-caption">Subtitle 1</div>
+        <div class="ma-10 d-flex justify-space-between">
+          <div style="width: 100px; height: 100px" class="pa-4 border rounded-lg elevation-2">
             teste
           </div>
-          <div style="width: 100px; height: 100px" class="border rounded-lg elevation-2">
+          <div style="width: 100px; height: 100px" class="pa-4 border rounded-lg elevation-2">
             teste
           </div>
-          <div style="width: 100px; height: 100px" class="border rounded-lg elevation-2">
+          <div style="width: 100px; height: 100px" class="pa-4 border rounded-lg elevation-2">
             teste
           </div>
         </div>
