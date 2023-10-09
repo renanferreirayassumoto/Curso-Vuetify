dfe79901c3c8ca78651cbbe16fa12fcd71439cd1
renanferreirayassumoto <renankosmo@gmail.com>
2023-10-09 10:27:02 -0400

    dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app :theme="cor">
     <v-navigation-drawer v-model="isDrawerOpen">
       <v-list>
         <v-list-subheader>Menu</v-list-subheader>
@@ -8,11 +8,7 @@
 
         <v-list-group value="Clientes">
           <template #activator="{ props }">
-            <v-list-item
-              v-bind="props"
-              prepend-icon="mdi-account-circle"
-              title="Clientes"
-            >
+            <v-list-item v-bind="props" prepend-icon="mdi-account-circle" title="Clientes">
             </v-list-item>
           </template>
 
@@ -22,12 +18,14 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar flat class="border-b">
-      <v-app-bar-nav-icon
-        @click="isDrawerOpen = !isDrawerOpen"
-      ></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen"></v-app-bar-nav-icon>
       <v-app-bar-title>Meu app</v-app-bar-title>
 
       <template #append>
+
+        <v-btn icon="mdi-theme-light-dark" class="mr-2" @click="alterarCor">
+        </v-btn>
+
         <v-btn icon class="mr-2">
           <v-badge dot color="info">
             <v-icon icon="mdi-bell-outline"></v-icon>
@@ -37,10 +35,8 @@
         <v-menu>
           <template #activator="{ props }">
             <v-avatar v-bind="props">
-              <v-img
-                cover
-                src="https://i0.wp.com/pediaa.com/wp-content/uploads/2021/09/Portrait.jpg?fit=427%2C640&ssl=1"
-              ></v-img>
+              <v-img cover
+                src="https://i0.wp.com/pediaa.com/wp-content/uploads/2021/09/Portrait.jpg?fit=427%2C640&ssl=1"></v-img>
             </v-avatar>
           </template>
 
@@ -65,41 +61,27 @@
           <div class="d-flex justify-space-between">
             <v-card-title>Últimos usuários</v-card-title>
             <v-card-title>
-              <v-btn @click="isDialogOpen = true" variant="tonal" size="small"
-                >Adicionar Usuário</v-btn
-              >
+              <v-btn @click="isDialogOpen = true" variant="tonal" size="small">Adicionar Usuário</v-btn>
               <v-dialog v-model="isDialogOpen" width="600px">
                 <v-card>
                   <v-card-title>Adicionar usuário</v-card-title>
                   <v-card-text>
                     <v-row>
                       <v-col>
-                        <v-text-field
-                          label="Nome"
-                          variant="outlined"
-                        ></v-text-field>
+                        <v-text-field label="Nome" variant="outlined"></v-text-field>
                       </v-col>
 
                       <v-col>
-                        <v-text-field
-                          label="Email"
-                          variant="outlined"
-                        ></v-text-field>
+                        <v-text-field label="Email" variant="outlined"></v-text-field>
                       </v-col>
                     </v-row>
 
-                    <v-select
-                      label="Cargo"
-                      variant="outlined"
-                      :items="['Admin', 'Gerente', 'Convidado']"
-                    ></v-select>
+                    <v-select label="Cargo" variant="outlined" :items="['Admin', 'Gerente', 'Convidado']"></v-select>
                   </v-card-text>
 
                   <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn variant="text" @click="isDialogOpen = false"
-                      >Cancelar</v-btn
-                    >
+                    <v-btn variant="text" @click="isDialogOpen = false">Cancelar</v-btn>
                     <v-btn variant="tonal" color="success">Salvar</v-btn>
                   </v-card-actions>
                 </v-card>
@@ -159,12 +141,8 @@
         <v-row>
           <v-col cols="12" sm="6" md="4" lg="3">
             <v-card flat class="border mx-auto" max-width="400">
-              <v-img
-                class="align-end text-white"
-                :height="200"
-                src="https://www.estudokids.com.br/wp-content/uploads/2020/02/o-que-e-paisagem-1200x675.jpg"
-                cover
-              >
+              <v-img class="align-end text-white" :height="200"
+                src="https://www.estudokids.com.br/wp-content/uploads/2020/02/o-que-e-paisagem-1200x675.jpg" cover>
                 <v-card-title>Top 10 paisagens naturais</v-card-title>
               </v-img>
               <v-card-subtitle class="mt-3">Ilhas Caribenhas</v-card-subtitle>
@@ -174,21 +152,15 @@
               </v-card-text>
               <v-card-actions>
                 <v-btn variant="outlined" color="primary">Ver mais</v-btn>
-                <v-btn prepend-icon="mdi-cart" variant="tonal" color="success"
-                  >Comprar</v-btn
-                >
+                <v-btn prepend-icon="mdi-cart" variant="tonal" color="success">Comprar</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
 
           <v-col cols="12" sm="6" md="4" lg="3">
             <v-card flat class="border mx-auto" max-width="400">
-              <v-img
-                class="align-end text-white"
-                :height="200"
-                src="https://www.estudokids.com.br/wp-content/uploads/2020/02/o-que-e-paisagem-1200x675.jpg"
-                cover
-              >
+              <v-img class="align-end text-white" :height="200"
+                src="https://www.estudokids.com.br/wp-content/uploads/2020/02/o-que-e-paisagem-1200x675.jpg" cover>
                 <v-card-title>Top 10 paisagens naturais</v-card-title>
               </v-img>
               <v-card-subtitle class="mt-3">Ilhas Caribenhas</v-card-subtitle>
@@ -198,21 +170,15 @@
               </v-card-text>
               <v-card-actions>
                 <v-btn variant="outlined" color="primary">Ver mais</v-btn>
-                <v-btn prepend-icon="mdi-cart" variant="tonal" color="success"
-                  >Comprar</v-btn
-                >
+                <v-btn prepend-icon="mdi-cart" variant="tonal" color="success">Comprar</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
 
           <v-col cols="12" sm="6" md="4" lg="3">
             <v-card flat class="border mx-auto" max-width="400">
-              <v-img
-                class="align-end text-white"
-                :height="200"
-                src="https://www.estudokids.com.br/wp-content/uploads/2020/02/o-que-e-paisagem-1200x675.jpg"
-                cover
-              >
+              <v-img class="align-end text-white" :height="200"
+                src="https://www.estudokids.com.br/wp-content/uploads/2020/02/o-que-e-paisagem-1200x675.jpg" cover>
                 <v-card-title>Top 10 paisagens naturais</v-card-title>
               </v-img>
               <v-card-subtitle class="mt-3">Ilhas Caribenhas</v-card-subtitle>
@@ -222,21 +188,15 @@
               </v-card-text>
               <v-card-actions>
                 <v-btn variant="outlined" color="primary">Ver mais</v-btn>
-                <v-btn prepend-icon="mdi-cart" variant="tonal" color="success"
-                  >Comprar</v-btn
-                >
+                <v-btn prepend-icon="mdi-cart" variant="tonal" color="success">Comprar</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
 
           <v-col cols="12" sm="6" md="4" lg="3">
             <v-card flat class="border mx-auto" max-width="400">
-              <v-img
-                class="align-end text-white"
-                :height="200"
-                src="https://www.estudokids.com.br/wp-content/uploads/2020/02/o-que-e-paisagem-1200x675.jpg"
-                cover
-              >
+              <v-img class="align-end text-white" :height="200"
+                src="https://www.estudokids.com.br/wp-content/uploads/2020/02/o-que-e-paisagem-1200x675.jpg" cover>
                 <v-card-title>Top 10 paisagens naturais</v-card-title>
               </v-img>
               <v-card-subtitle class="mt-3">Ilhas Caribenhas</v-card-subtitle>
@@ -246,9 +206,7 @@
               </v-card-text>
               <v-card-actions>
                 <v-btn variant="outlined" color="primary">Ver mais</v-btn>
-                <v-btn prepend-icon="mdi-cart" variant="tonal" color="success"
-                  >Comprar</v-btn
-                >
+                <v-btn prepend-icon="mdi-cart" variant="tonal" color="success">Comprar</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
@@ -271,7 +229,17 @@ export default {
   components: {},
 
   data: () => ({
-    //
+    cor: 'white'
   }),
+  methods: {
+    alterarCor() {
+      if (this.cor == 'white') {
+        this.cor = 'dark';
+      }
+      else {
+        this.cor = 'white';
+      }
+    }
+  }
 };
 </script>
